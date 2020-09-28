4e40ec51eb1a98ae99252d59cd84b23b4f98a868
unqvsn <mmichniak@gmail.com>
2020-09-28 10:29:53 +0200

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,52 @@
-This attachment was removed.
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+						options: {
+							limit: false,
+						},
+                    },
+                ],
+            },
+			{
+				test: /\.html$/,
+				loader: 'html-loader',
+			}
+        ],
+    },
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: path.resolve('./dist/index.html'),
+			template: path.resolve('./src/index.html'),
+		})
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+	}
+};
+
+module.exports = config;
